579376731be330e60ead8a1ac76735f097a259e9
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2020-01-18 14:13:01 +0100

    Recursive vs iterative function

---
+++ Tests/RecursiveFunctionsTests.cs
@@ -0,0 +1,73 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class RecursiveFunctionsTests
+    {
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 1)]
+        public void Recursive_base_should_produce_expected_result(uint x, uint expected)
+        {
+            var sut = new Code();
+
+            var result = sut.FactorialRecursive(x);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        [InlineData(10, 3628800)]
+        public void Recursive_should_produce_expected_result(uint x, uint expected)
+        {
+            var sut = new Code();
+
+            var result = sut.FactorialRecursive(x);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        [InlineData(10, 3628800)]
+        public void Iterative_should_produce_expected_result(uint x, uint expected)
+        {
+            var sut = new Code();
+
+            var result = sut.FactorialIteration(x);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class Code
+    {
+        public uint FactorialRecursive(uint x)
+        {
+            if (x <= 1)
+            {
+                return 1;
+            }
+
+            return x * FactorialRecursive(x - 1);
+        }
+
+        public uint FactorialIteration(uint x)
+        {
+            uint result = 1;
+            for (uint i = 2; i <= x; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
