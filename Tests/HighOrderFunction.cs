4b24c77757b787779ab0adf25129f4e76972683c
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2020-01-27 19:43:20 +0100

    Higher-order functions demo

---
+++ Tests/HighOrderFunction.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+    public class HighOrderFunctionsTest
+    {
+        private readonly ITestOutputHelper _output;
+
+        public HighOrderFunctionsTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void SumLambda_Test()
+        {
+            var expected = 3;
+
+            var func = Sum();
+
+            var result = func(1, 2);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void SumDelegate_Test()
+        {
+            var expected = 3;
+
+            var func = SumDelegate();
+
+            var result = func(1, 2);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void SumFunction_Test()
+        {
+            var expected = 3;
+
+            var func = SumFunction();
+            var result = func(1, 2);
+
+            Assert.Equal(expected, result);
+        }
+
+        ///////////////////////////////////////////
+
+        public Func<int, int, int> Sum()
+        {
+            return (x, y) => x + y;
+        }
+
+        public Func<int, int, int> SumDelegate()
+        {
+            return delegate (int x, int y) { return x + y; };
+        }
+
+        public Func<int, int, int> SumFunction()
+        {
+            return SumOfTwoInteger;
+        }
+
+        private static int SumOfTwoInteger(int x, int y)
+        {
+            return x + y;
+        }
+
+        ///////////////////////////////////////////
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(10)]
+        [InlineData(100)]
+        public void FunctionAsAParameter_Test(int p)
+        {
+            static int Random(int max) => new Random().Next(max);
+
+            var result = IsEven(Random, p) ? "even" : "odd";
+
+            _output.WriteLine($"{result}");
+        }
+
+        ///////////////////////////////////////////
+
+        private bool IsEven(Func<int, int> func, int max)
+        {
+            var r = func(max);
+            _output.WriteLine($"{r}");
+
+            return r % 2 == 0;
+        }
+
+        ///////////////////////////////////////////
+
+    }
+}
